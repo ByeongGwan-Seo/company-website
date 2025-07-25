804cd94fb98e5c4f35d0dbfdcf3ac6fd0b07d37e
ByeongGwan-Seo <seo.jp.work@gmail.com>
2025-07-25 10:08:26 +0900

    mongoose setting

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3000;
@@ -6,6 +8,13 @@ const PORT = 3000;
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 app.listen(PORT, () => {
   console.log("server is running");
