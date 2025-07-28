53a260bb82a20a7951d73fd6bcd058d6af76c057
ByeongGwan-Seo <seo.jp.work@gmail.com>
2025-07-28 11:11:01 +0900

    admin crud done

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+const userRoutes = require("./routes/user");
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.use("/api/auth", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
