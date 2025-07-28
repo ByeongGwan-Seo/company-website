dcf66f6683cfb777add95d3aa0bbffa7598fc79e
ByeongGwan-Seo <seo.jp.work@gmail.com>
2025-07-28 16:44:30 +0900

    contact model design

---
+++ backend/models/Contact.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["in_progress", "pending", "completed"],
+      default: "in_progress",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Contact = mongoose.mondel("Contact", contactSchema);
+module.exports = Contact;
